b692e4f84185dcb9f713bd1105e77d7b07cd5ad7
Piotr Masior <fokstrot76@gmail.com>
2015-12-03 09:39:09 +0000

    Unity Test Tool
    I imported unity test tool

---
+++ Assets/Editor/PlayerHealth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+public class PlayerHealth  {
+	[Test]
+	public void TestHealthStartsAtOne(){
+		//PlayerHealth playerHealth = new PlayerHealth ();
+		PlayerLives playerHealth = new PlayerLives();
+		int expectedHealth = 1;
+		int actualHealth = playerHealth.currentHealth1;
+		Assert.AreEqual (expectedHealth, actualHealth);
+	}
+	[Test]
+	 public void IgnoredTest()
+	 {
+		throw new Exception("Ignored this test");
+	 }
+}
